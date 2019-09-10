0273309031167d6d7cdb0509ccffff78435cad62
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-09-10 15:17:10 +0530

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageHolderComponent } from './page-holder/page-holder.component';
+import { PollItemComponent } from './poll-item/poll-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageHolderComponent,
+    PollItemComponent
   ],
   imports: [
     BrowserModule
