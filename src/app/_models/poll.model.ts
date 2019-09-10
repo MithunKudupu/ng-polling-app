095c3b97ae54e6b10adcafbe1f72833a8b67003d
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-09-10 15:50:38 +0530

    created array and used ngFor

---
+++ src/app/_models/poll.model.ts
@@ -0,0 +1,6 @@
+export class PollItem {
+  id: number;
+  title: string;
+  description: string;
+  voteCount = 0;
+}
--- src/app/page-holder/page-holder.component.ts
+++ src/app/page-holder/page-holder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PollItem } from '../_models/poll.model';
 
 @Component({
   selector: 'app-page-holder',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-holder.component.css']
 })
 export class PageHolderComponent implements OnInit {
+  pollItemCollection: PollItem[] = [
+    {
+      id: 1,
+      title: 'Sofa',
+      description: 'Nice comfortable sofas to sit and relax',
+      voteCount: 0
+    },
+    {
+      id: 2,
+      title: 'Foosball',
+      description: 'Lets play and enjoy!',
+      voteCount: 0
+    },
+    {
+      id: 3,
+      title: 'Gym',
+      description: 'Here is the fitness place for you!',
+      voteCount: 0
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
